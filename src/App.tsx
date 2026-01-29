c4c5fec410ca4f017d0aeccf9b4c60f7995a17da
ziad <zizogamal619@yahoo.com>
2026-01-29 13:36:38 +0200

    added images

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import "./App.css";
 import { useState } from "react";
+import { data } from "./assets.js";
 function App() {
   const [start, setStart] = useState(false);
+  const randomData = data.sort(() => Math.random() - 0.5);
   return (
     <>
-      <canvas className="w-(--width) h-(--height) border-4 border-black rounded-xl "></canvas>
+      <div className="w-(--width) h-(--height) pb-8 pt-8 border-4 border-black rounded-xl ">
+        <div className="grid grid-cols-4 gap-3 grid-rows-3  items-center justify-items-center w-full h-full ">
+          {randomData.map((item) => (
+            <div key={item.id} className="w-[200px] h-fit  ">
+              <img
+                src={item.src}
+                alt={item.alt}
+                className="w-full h-full object-cover rounded-full"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
       <div className="flex gap-3 justify-center mt-3">
         <button className="p-3 bg-blue-400  rounded-2xl hover:bg-blue-500 cursor-pointer hover:scale-110 transition-all duration-300">
           Start game
--- src/assets.js
+++ src/assets.js
@@ -1,58 +1,74 @@
-const { type } = require("os");
-
 export const data = [
   {
     id: 1,
-    src:"./assets/avian.jfif",
-    alt:"avian",
-    type:"image/jfif"
+    src: "../assets/avian.jfif",
+    alt: "avian",
+    type: "image/jfif",
   },
   {
     id: 2,
-    src:"./assets/bubbleman.jfif",
-    alt:"bubbleman",
-    type:"image/jfif"
-  },{
+    src: "../assets/bubbleman.jfif",
+    alt: "bubbleman",
+    type: "image/jfif",
+  },
+  {
     id: 3,
-    src:"./assets/burstinatrix.jpg",
-    alt:"burstinatrix",
-    type:"image/jpg"
-  },{
+    src: "../assets/burstinatrix.jpg",
+    alt: "burstinatrix",
+    type: "image/jpg",
+  },
+  {
     id: 4,
-    src:"./assets/ElementalHEROFlameWingman-OW.webp",
-    alt:"FlameWingman",
-    type:"image/webp"
-  },{
-    id:5,
-    src:"./assets/windxyz.jpg",
-    alt:"windxyz",
-    type:"image/jpg"
-  },  {
+    src: "../assets/ElementalHEROFlameWingman-OW.webp",
+    alt: "FlameWingman",
+    type: "image/webp",
+  },
+  {
+    id: 5,
+    src: "../assets/windxyz.jpg",
+    alt: "windxyz",
+    type: "image/jpg",
+  },
+  {
     id: 6,
-    src:"./assets/avian.jfif",
-    alt:"avian",
-    type:"image/jfif"
+    src: "../assets/avian.jfif",
+    alt: "avian",
+    type: "image/jfif",
   },
   {
     id: 7,
-    src:"./assets/bubbleman.jfif",
-    alt:"bubbleman",
-    type:"image/jfif"
-  },{
+    src: "../assets/bubbleman.jfif",
+    alt: "bubbleman",
+    type: "image/jfif",
+  },
+  {
     id: 8,
-    src:"./assets/burstinatrix.jpg",
-    alt:"burstinatrix",
-    type:"image/jpg"
-  },{
+    src: "../assets/burstinatrix.jpg",
+    alt: "burstinatrix",
+    type: "image/jpg",
+  },
+  {
     id: 9,
-    src:"./assets/ElementalHEROFlameWingman-OW.webp",
-    alt:"FlameWingman",
-    type:"image/webp"
-  },{
-    id:10,
-    src:"./assets/windxyz.jpg",
-    alt:"windxyz",
-    type:"image/jpg"
-  }
-
+    src: "../assets/ElementalHEROFlameWingman-OW.webp",
+    alt: "FlameWingman",
+    type: "image/webp",
+  },
+  {
+    id: 10,
+    src: "../assets/windxyz.jpg",
+    alt: "windxyz",
+    type: "image/jpg",
+  },
+  {
+    id: 11,
+    src: "../assets/pot_of_greed.jpg",
+    alt: "pot_of_greed",
+    type: "image/jpg",
+  },
+  {
+    id: 12,
+    src: "../assets/pot_of_greed.jpg",
+    alt: "pot_of_greed",
+    type: "image/jpg",
+  },
 ];
