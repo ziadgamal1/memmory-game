2602096e28537d415dfb3edecb6bebb8444f9fdf
ziad <zizogamal619@yahoo.com>
2026-02-22 17:29:34 +0200

    here we go!

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import "./App.css";
 import { useState } from "react";
 import { data } from "./assets.js";
+import type { DataItem } from "./types/index";
 function App() {
-  const [start, setStart] = useState(false);
+  const [start, setStart] = useState<boolean>(false);
   const randomData = data.sort(() => Math.random() - 0.5);
   return (
     <>
       <div className="w-(--width) h-(--height) pb-8 pt-8 border-4 border-black rounded-xl ">
-        <div className="grid grid-cols-4 gap-3 grid-rows-3  items-center justify-items-center w-full h-full ">
-          {randomData.map((item) => (
-            <div key={item.id} className="w-[200px] h-fit  ">
+        <div className="grid grid-cols-4 gap-3 grid-rows-3  items-center justify-items-center w-full h-full relative ">
+          {randomData.map((item: DataItem) => (
+            <div key={item.id} className="w-50 h-fit  ">
               <img
                 src={item.src}
                 alt={item.alt}
                 className="w-full h-full object-cover rounded-full"
               />
+              <span className={`back`}></span>
             </div>
           ))}
         </div>
